405d8110d7a5b562a83c0b8b350d5f42d8ba0f88
my_firsthub <2414025817@qq.com>
2018-08-12 16:15:03 +0800

    测试提交index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 echo "hello github!";
+echo 
+"
+	git init
+	git add index.php
+	git commit -m '测试提交index.php'
+	git remote add origin https://github.com/zhumengqun/my_firsthub.git
+	git push -u origin master
+"
